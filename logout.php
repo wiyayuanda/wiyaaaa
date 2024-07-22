ba270a21d3724272defe12d1408e1a95cc079c44
wiyayuanda <wiyayuliandari@gmail.com>
2024-07-23 02:42:11 +0800

    Add files via upload

---
+++ logout.php
@@ -0,0 +1,29 @@
+<?php 
+
+session_start();
+
+if (!isset($_SESSION["akun-admin"]) && !isset($_SESSION["akun-user"])) {
+
+    header("Location: login.php");
+
+    exit;
+
+} 
+
+
+
+session_unset();
+
+session_destroy();
+
+
+
+?>
+
+<script>
+
+    alert('Berhasil Keluar!');
+
+    location.href = 'login.php';
+
+</script>
